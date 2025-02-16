9b4bc90a7ad8ea96a65b8f40923b0ff0be0aabde
trifonovsv <stnslvtrfnv@ya.ru>
2025-02-17 00:32:09 +0400

    add CPUUsageToJson method for grafana

--- web_plugin/api_package/schemas/__init__.py
+++ web_plugin/api_package/schemas/__init__.py
@@ -1,5 +1,5 @@
 from flask_restx import fields
-
+from typing import List
 
 RETURN_HW_DATA_SCHEMA_POST = {"start_time":fields.String(),
                               "end_time":fields.String()}
@@ -8,6 +8,7 @@ RETURN_HW_DATA_SCHEMA_POST = {"start_time":fields.String(),
 COMMON_RETURN_SCHEMA = {"message":fields.String(),
                         "error":fields.Boolean(),
                         "data":fields.Raw()}
+RAW_DATA_SCHEMA = List
 
 STOP_TELEMETRY_COLLECTION_SCHEMA_GET = {"message":fields.String(),
                                         "error":fields.Boolean()}
--- web_plugin/api_package/telemetry_ns.py
+++ web_plugin/api_package/telemetry_ns.py
@@ -1,16 +1,22 @@
+import json
+
+
+import tempfile
 import subprocess
 from datetime import datetime
 from os import mkdir
 
 import psutil
-from flask import request
+from flask import request, send_file
 from flask_restx import Resource, Namespace
 from flask_cors import cross_origin
 
 from env.default_env import PATH_TO_PYTHON_EXE, LOG_DIR
 from stored_data_operation import DataObject
+from utils.file_remover import FileRemover
 
-from web_plugin.api_package.schemas import RUN_TELEMETRY_COLLECTION_SCHEMA_GET, STOP_TELEMETRY_COLLECTION_SCHEMA_GET, PERIOD_REQUEST_DATA, COMMON_RETURN_SCHEMA
+from web_plugin.api_package.schemas import RUN_TELEMETRY_COLLECTION_SCHEMA_GET, STOP_TELEMETRY_COLLECTION_SCHEMA_GET, \
+    PERIOD_REQUEST_DATA, COMMON_RETURN_SCHEMA
 
 import os
 import sys
@@ -27,6 +33,82 @@ logger_instance = CustomLogger(logger_name="telemetry_ns",
 my_logger = logger_instance.logger
 
 telemetry_ns = Namespace('telemetry', description='access to host telemetry data')
+
+
+@telemetry_ns.route("/cpu_usage_to_json")
+@telemetry_ns.doc(params={"start_time":{"description":"start of request period",
+                                        "type": "str"},
+                          "end_time":{"description":"end of request period",
+                                      "type": "str"}})
+class CPUUsageToJson(Resource):
+    """return json file contained cpu usage data with query in params"""
+    #TODO Удалять временный файл после отправки по запросу
+    @cross_origin()
+    @telemetry_ns.doc('return json file contained cpu usage data')
+    # @telemetry_ns.expect(telemetry_ns.model('cpu_usage_data_post', PERIOD_REQUEST_DATA))
+    # @telemetry_ns.marshal_with(telemetry_ns.model('cpu_usage_schema', RAW_DATA_SCHEMA))
+    def get(self):
+        start = request.args.get('start_time')
+        end = request.args.get('end_time')
+
+        try:
+            if datetime.fromisoformat(start)>datetime.fromisoformat(end):
+                raise ValueError("Время начала периода позже его окончания")
+        except ValueError as val_err:
+            my_logger.error(f"{request.url} (POST) ValueError: \n{val_err}")
+            telemetry_ns.abort(400, f"ValueError {str(val_err)}")
+        try:
+            df = DataObject(start, end)
+            return_data = df.get_cpu_usage()
+            tempdir = tempfile.mkdtemp()
+            fp = tempfile.NamedTemporaryFile(mode='w+t',
+                                             suffix='.json',
+                                             delete = False,
+                                             dir=tempdir)
+            fp.write(json.dumps(return_data))
+
+            resp = send_file(fp.name, as_attachment=True)
+            file_remover = FileRemover()
+            fp.close()
+            file_remover.cleanup_once_done(resp, tempdir)
+            return resp, 200
+
+        except Exception as err:
+            my_logger.error(f"{request.url} (POST) error: \n{err}")
+            telemetry_ns.abort(400, f"Another problem occurred: {str(err)}")
+
+
+@telemetry_ns.route("/cpu_usage")
+@telemetry_ns.doc(params={"start_time":{"description":"start of request period",
+                                        "type": "str"},
+                          "end_time":{"description":"end of request period",
+                                      "type": "str"}})
+class CPUUsage(Resource):
+    """return cpu usage data with query in params"""
+    @cross_origin()
+    @telemetry_ns.doc('return cpu usage data')
+    # @telemetry_ns.expect(telemetry_ns.model('cpu_usage_data_post', PERIOD_REQUEST_DATA))
+    # @telemetry_ns.marshal_with(telemetry_ns.model('cpu_usage_schema', RAW_DATA_SCHEMA))
+    def get(self):
+        start = request.args.get('start_time')
+        end = request.args.get('end_time')
+
+        try:
+            if datetime.fromisoformat(start)>datetime.fromisoformat(end):
+                raise ValueError("Время начала периода позже его окончания")
+        except ValueError as val_err:
+            my_logger.error(f"{request.url} (POST) ValueError: \n{val_err}")
+            telemetry_ns.abort(400, f"ValueError {str(val_err)}")
+        try:
+            df = DataObject(start, end)
+            return_data = df.get_cpu_usage()
+            return_body = return_data
+            return return_body, 200
+        except Exception as err:
+            my_logger.error(f"{request.url} (POST) error: \n{err}")
+            telemetry_ns.abort(400, f"Another problem occurred: {str(err)}")
+
+
 
 @telemetry_ns.route("/cpu_usage_data")
 class CPUUsageData(Resource):
