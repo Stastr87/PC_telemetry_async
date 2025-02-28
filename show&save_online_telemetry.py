21f8128b6952d042171d8365b51f6a1145607cd2
Stastr87 <80085845+Stastr87@users.noreply.github.com>
2025-03-01 02:25:25 +0400

    Add files via upload
    refactor

--- show&save_online_telemetry.py
+++ show&save_online_telemetry.py
@@ -1,4 +1,6 @@
 import curses, canvas_data
+import sys
+import time
 
 
 def draw(canvas):
@@ -11,58 +13,60 @@ def draw(canvas):
     # не требуя нажатия клавиши Enter; это называется режимом cbreak, 
     # в отличие от обычного режима ввода с буферизацией.
     curses.cbreak()
-    
+    # Обновление холста будет работать автоматически без ожидания нажатий клавиш
+    canvas.nodelay(True)
     # Добавим набор стилей для отображения текста
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    
+
+    display_data = canvas_data.get_txt()
     while True:
-        canvas.clear()
-        canvas.border()
-        # Отключить мигающий курсор
-        curses.curs_set(False)
+        try:
+            canvas.clear()
+            canvas.border()
+            # Отключить мигающий курсор
+            curses.curs_set(False)
 
-        height,width = canvas.getmaxyx()
-        
-        # Зададим значение главного отступа для всех строк main_offset
-        main_offset = 2
+            height,width = canvas.getmaxyx()
 
-        # Получим данные для отображения на холсте
-        display_data = canvas_data.get_txt()
+            # Зададим значение главного отступа для всех строк main_offset
+            main_offset = 2
 
-        # Построчно разместим в окне
-        rows = display_data.split('\n')
-        
-        # Для того что бы окно не падало с ошибкой добавим заглушку для отображения
-        # найдем самую длинную строку
-        row_len_list = list(map(lambda x: len(x),rows))
-        min_width = max(row_len_list)
-        
-        if width<min_width:
-            canvas.addstr(1, 1, f'min width {min_width}')
-            canvas.addstr(2, 1, f'cur width {width}')
-        
-        else:
-            for row in range(len(rows)):
-                if row==0 or row==2:
-                    canvas.addstr(row+1, main_offset+round(min_width/2)-round(len(rows[row])/2), rows[row])
-                else:
-                    canvas.addstr(row+1, main_offset, rows[row])
+            # Построчно разместим в окне
+            rows = display_data.split('\n')
 
-        canvas.refresh()
-        # time.sleep(1)
-        
-        # Обновление холста будет работать автоматически без ожидания нажатий клавиш
-        canvas.nodelay(True)
-        key = canvas.getch()
-        
-        if key == 27:
-            curses.nocbreak()
-            canvas.keypad(False)
-            curses.echo()
-            # endwin - завершает сессию и возвращает в обычную консоль
-            curses.endwin()
-            print(f"EXIT")
-            quit()
+            # Для того что бы окно не падало с ошибкой добавим заглушку для отображения
+            # найдем самую длинную строку
+            row_len_list = list(map(lambda x: len(x),rows))
+            min_width = max(row_len_list)
+
+            if width<min_width:
+                canvas.addstr(1, 1, f'min width {min_width}')
+                canvas.addstr(2, 1, f'cur width {width}')
+
+            else:
+                for row in range(len(rows)):
+                    if row==0 or row==2:
+                        canvas.addstr(row+1, main_offset+round(min_width/2)-round(len(rows[row])/2), rows[row])
+                    else:
+                        canvas.addstr(row+1, main_offset, rows[row])
+
+            canvas.refresh()
+            # Холст обновляется с частотой 1Гц
+            time.sleep(1)
+            display_data = canvas_data.get_txt()
+
+            key = canvas.getch()
+            if key == 27:
+                curses.nocbreak()
+                canvas.keypad(False)
+                curses.echo()
+                # endwin - завершает сессию и возвращает в обычную консоль
+                curses.endwin()
+                print(f"EXIT")
+                sys.exit()
+
+        except KeyboardInterrupt:
+            pass
 
 
   
